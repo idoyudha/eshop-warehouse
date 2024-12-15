4791797c5c445d835890de91904a7ba6c1b72900
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-15 16:13:14 +0700

    feat: config

---
+++ config/config.go
@@ -0,0 +1,53 @@
+package config
+
+import "github.com/ilyakaznacheev/cleanenv"
+
+type (
+	Config struct {
+		App  `yaml:"app"`
+		HTTP `yaml:"http"`
+		Log  `yaml:"log"`
+		PostgreSQL
+		AuthService
+	}
+
+	App struct {
+		Name    string `env-required:"true" yaml:"name" env:"APP_NAME"`
+		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
+	}
+
+	HTTP struct {
+		Port string `env-required:"true" yaml:"port" env:"HTTP_PORT"`
+	}
+
+	PostgreSQL struct {
+		URL         string `env-required:"true" env:"POSTGRESQL_URL"`
+		ConnTimeout int    `env-required:"true" env:"POSTGRESQL_CONN_TIMEOUT"`
+		ConnAttemps int    `env-required:"true" env:"POSTGRESQL_CONN_ATTEMPS"`
+		MaxPoolSize int    `env-required:"true" env:"POSTGRESQL_MAX_POOL_SIZE"`
+	}
+
+	Log struct {
+		Level string `env-required:"true" yaml:"level" env:"LOG_LEVEL"`
+	}
+
+	AuthService struct {
+		BaseURL string `env-required:"true" env:"AUTH_SERVICE"`
+	}
+)
+
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+
+	err := cleanenv.ReadConfig("./config/config.yml", cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	err = cleanenv.ReadEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/idoyudha/eshop-warehouse
 go 1.23.4
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/bytedance/sonic v1.12.5 // indirect
 	github.com/bytedance/sonic/loader v0.2.1 // indirect
 	github.com/cloudwego/base64x v0.1.4 // indirect
@@ -15,6 +16,8 @@ require (
 	github.com/go-playground/validator/v10 v10.23.0 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
@@ -33,4 +36,5 @@ require (
 	golang.org/x/text v0.21.0 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
