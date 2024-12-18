99592b0c9d2811b0452ba53fe5e1fa3a0a4f13ef
idoyudha <idowidya.yudhatama@gmail.com>
2024-12-18 17:38:57 +0700

    feat: warehouse ranks redis implementation repository

---
+++ internal/entity/warehouse_distance.go
@@ -0,0 +1,6 @@
+package entity
+
+type WarehouseDistance struct {
+	WarehouseID string  `json:"warehouse_id"`
+	Distance    float64 `json:"distance"`
+}
--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -8,21 +8,25 @@ import (
 )
 
 type (
+	WarehouseRankRedisRepo interface {
+		UpdateWarehouseRanks(context.Context, string, string, map[string]float64) error
+		GetNearestWarehouses(context.Context, string) ([]entity.WarehouseDistance, error)
+	}
 	WarehousePostgreRepo interface {
-		Save(ctx context.Context, warehouse *entity.Warehouse) error
-		Update(ctx context.Context, warehouse *entity.Warehouse) error
-		GetByID(ctx context.Context, id uuid.UUID) (*entity.Warehouse, error)
-		GetAll(ctx context.Context) ([]*entity.Warehouse, error)
-		GetAllExceptMain(ctx context.Context) ([]*entity.Warehouse, error)
+		Save(context.Context, *entity.Warehouse) error
+		Update(context.Context, *entity.Warehouse) error
+		GetByID(context.Context, uuid.UUID) (*entity.Warehouse, error)
+		GetAll(context.Context) ([]*entity.Warehouse, error)
+		GetAllExceptMain(context.Context) ([]*entity.Warehouse, error)
 	}
 
 	WarehouseProductPostgreRepo interface {
-		Save(ctx context.Context, warehouseProduct *entity.WarehouseProduct) error
-		UpdateProductQuantity(ctx context.Context, warehouseProduct *entity.WarehouseProduct) error
-		GetAll(ctx context.Context) ([]*entity.WarehouseProduct, error)
-		GetByProductID(ctx context.Context, id uuid.UUID) (*entity.WarehouseProduct, error)
-		GetByWarehouseID(ctx context.Context, id uuid.UUID) ([]*entity.WarehouseProduct, error)
-		GetByProductIDAndWarehouseID(ctx context.Context, productID uuid.UUID, warehouseID uuid.UUID) (*entity.WarehouseProduct, error)
+		Save(context.Context, *entity.WarehouseProduct) error
+		UpdateProductQuantity(context.Context, *entity.WarehouseProduct) error
+		GetAll(context.Context) ([]*entity.WarehouseProduct, error)
+		GetByProductID(context.Context, uuid.UUID) (*entity.WarehouseProduct, error)
+		GetByWarehouseID(context.Context, uuid.UUID) ([]*entity.WarehouseProduct, error)
+		GetByProductIDAndWarehouseID(context.Context, uuid.UUID, uuid.UUID) (*entity.WarehouseProduct, error)
 	}
 
 	StockMovementPostgreRepo interface {
@@ -33,24 +37,24 @@ type (
 	}
 
 	TransactionProductPostgresRepo interface {
-		TransferIn(ctx context.Context, stockMovement *entity.StockMovement, warehouseProduct *entity.WarehouseProduct) error
-		TransferOut(ctx context.Context, stockMovement []*entity.StockMovement, warehouseProduct *entity.WarehouseProduct) error
+		TransferIn(context.Context, *entity.StockMovement, *entity.WarehouseProduct) error
+		TransferOut(context.Context, []*entity.StockMovement, *entity.WarehouseProduct) error
 	}
 
 	Warehouse interface {
-		CreateWarehouse(ctx context.Context, warehouse *entity.Warehouse) error
-		UpdateWarehouse(ctx context.Context, warehouse *entity.Warehouse) error
-		GetWarehouseByID(ctx context.Context, id uuid.UUID) (*entity.Warehouse, error)
-		GetAllWarehouses(ctx context.Context) ([]*entity.Warehouse, error)
+		CreateWarehouse(context.Context, *entity.Warehouse) error
+		UpdateWarehouse(context.Context, *entity.Warehouse) error
+		GetWarehouseByID(context.Context, uuid.UUID) (*entity.Warehouse, error)
+		GetAllWarehouses(context.Context) ([]*entity.Warehouse, error)
 	}
 
 	WarehouseProduct interface {
-		CreateWarehouseProduct(ctx context.Context, warehouseProduct *entity.WarehouseProduct) error
-		UpdateWarehouseProduct(ctx context.Context, warehouseProduct *entity.WarehouseProduct) error
-		GetAllWarehouseProducts(ctx context.Context) ([]*entity.WarehouseProduct, error)
-		GetWarehouseProductByProductID(ctx context.Context, id uuid.UUID) (*entity.WarehouseProduct, error)
-		GetWarehouseProductByWarehouseID(ctx context.Context, id uuid.UUID) ([]*entity.WarehouseProduct, error)
-		GetWarehouseProductByProductIDAndWarehouseID(ctx context.Context, productID uuid.UUID, warehouseID uuid.UUID) (*entity.WarehouseProduct, error)
+		CreateWarehouseProduct(context.Context, *entity.WarehouseProduct) error
+		UpdateWarehouseProduct(context.Context, *entity.WarehouseProduct) error
+		GetAllWarehouseProducts(context.Context) ([]*entity.WarehouseProduct, error)
+		GetWarehouseProductByProductID(context.Context, uuid.UUID) (*entity.WarehouseProduct, error)
+		GetWarehouseProductByWarehouseID(context.Context, uuid.UUID) ([]*entity.WarehouseProduct, error)
+		GetWarehouseProductByProductIDAndWarehouseID(context.Context, uuid.UUID, uuid.UUID) (*entity.WarehouseProduct, error)
 	}
 
 	StockMovement interface {
