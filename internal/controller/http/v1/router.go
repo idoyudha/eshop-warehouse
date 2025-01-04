9cb645c9df8214ff9b922f392a3264cb09050edc
idoyudha <idowidya.yudhatama@gmail.com>
2025-01-04 08:27:36 +0700

    feat: add cors

--- internal/controller/http/v1/router.go
+++ internal/controller/http/v1/router.go
@@ -3,6 +3,7 @@ package v1
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/idoyudha/eshop-warehouse/config"
 	"github.com/idoyudha/eshop-warehouse/internal/usecase"
@@ -18,6 +19,14 @@ func NewRouter(
 	l logger.Interface,
 	auth config.AuthService,
 ) {
+	handler.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:3001"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * 3600,
+	}))
 
 	handler.GET("/health", func(c *gin.Context) {
 		c.Status(http.StatusOK)
